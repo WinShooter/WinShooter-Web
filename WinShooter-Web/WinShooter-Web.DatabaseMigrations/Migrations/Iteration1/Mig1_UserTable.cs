b132b823f0fb001e74a7ff8e1df33284fe01501e
Jonas Fredriksson <jonas.fredriksson@tradera.com>
2013-07-04 13:08:25 +0200

    Adding assembly that will be used in database migrations

---
+++ WinShooter-Web/WinShooter-Web.DatabaseMigrations/Migrations/Iteration1/Mig1_UserTable.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace WinShooter_Web.DatabaseMigrations.Migrations.Iteration1
+{
+    [Migration(1)]
+    public class Mig1_UserTable : Migration
+    {
+        public override void Up()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Down()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
