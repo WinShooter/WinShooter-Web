33b7bc87a8faba1c3b9e4096304a1c8efeb08d21
John Allberg <john_allberg@hotmail.com>
2013-07-06 04:22:39 +0200

    Started adding up everything to get a working MVC mode.

---
+++ WinShooter-Web/WinShooter-Web/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿
+namespace WinShooter_Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// The home controller.
+    /// </summary>
+    public class HomeController
+    {
+    }
+}
