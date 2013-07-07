4f08bf9489f6b0d9a76205fc615a156d86e81f88
John Allberg <john_allberg@hotmail.com>
2013-07-07 19:34:52 +0200

    Cleanup.

--- WinShooter-Web/App_Start/RouteConfig.cs
+++ WinShooter-Web/App_Start/RouteConfig.cs
@@ -17,8 +17,7 @@ namespace WinShooter
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
         }
     }
 }
