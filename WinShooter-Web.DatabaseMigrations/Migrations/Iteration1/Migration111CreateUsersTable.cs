e1f01ca1b39e279fb45d75edd76375dbdd9eb101
John Allberg <john_allberg@hotmail.com>
2013-07-18 07:32:20 +0200

    Added migration for creating users table and users login info table.

---
+++ WinShooter-Web.DatabaseMigrations/Migrations/Iteration1/Migration111CreateUsersTable.cs
@@ -0,0 +1,83 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Migration111CreateUsersTable.cs" company="Copyright ©2013 John Allberg & Jonas Fredriksson">
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU General Public License
+//   as published by the Free Software Foundation; either version 2
+//   of the License, or (at your option) any later version.
+//   
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General Public License for more details.
+//   
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the Free Software
+//   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// </copyright>
+// <summary>
+//   Creates the users users table.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WinShooter_Web.DatabaseMigrations.Migrations.Iteration1
+{
+    using FluentMigrator;
+
+    /// <summary>
+    /// Creates the users users table.
+    /// </summary>
+    [Migration(111)]
+    public class Migration111CreateUsersTable : Migration
+    {
+        /// <summary>
+        /// The users table name.
+        /// </summary>
+        private const string UsersTableName = "Users";
+
+        /// <summary>
+        /// The users login info table name.
+        /// </summary>
+        private const string UsersLoginInfoTableName = "UsersLoginInfo";
+
+        /// <summary>
+        /// The name of the foreign key between <see cref="UsersTableName"/> and <see cref="UsersLoginInfoTableName"/> tables.
+        /// </summary>
+        private const string UsersLoginInfoForeignKeyName = "FK_Users_UsersLoginInfo";
+
+        /// <summary>
+        /// Migrates the database up.
+        /// </summary>
+        public override void Up()
+        {
+            Create.Table(UsersTableName)
+                .WithColumn("Id").AsGuid().PrimaryKey().Indexed()
+                .WithColumn("CardNumber").AsString()
+                .WithColumn("Surname").AsString()
+                .WithColumn("Givenname").AsString()
+                .WithColumn("Email").AsString()
+                .WithColumn("ClubId").AsGuid()
+                .WithColumn("LastUpdated").AsDateTime()
+                .WithColumn("LastLogin").AsDateTime();
+
+            Create.Table(UsersLoginInfoTableName)
+                .WithColumn("Id").AsGuid().PrimaryKey().Indexed()
+                .WithColumn("UserId").AsGuid()
+                .WithColumn("IdentityProvider").AsString().Indexed()
+                .WithColumn("IdentityProviderId").AsString().Indexed();
+
+            Create.ForeignKey(UsersLoginInfoForeignKeyName)
+                .FromTable(UsersLoginInfoTableName).ForeignColumn("UserId")
+                .ToTable(UsersTableName).PrimaryColumn("Id");
+        }
+
+        /// <summary>
+        /// Migrates the database down.
+        /// </summary>
+        public override void Down()
+        {
+            Delete.ForeignKey(UsersLoginInfoForeignKeyName);
+            Delete.Table(UsersTableName);
+            Delete.Table(UsersLoginInfoTableName);
+        }
+    }
+}
