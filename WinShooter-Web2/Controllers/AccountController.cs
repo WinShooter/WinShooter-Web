02aaf8287b25326edfaf6ff1f8307315437e565e
John Allberg <john_allberg@hotmail.com>
2013-07-26 23:20:58 +0200

    Build a complete new web site from an empty website.

---
+++ WinShooter-Web2/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿namespace WinShooter.Controllers
+{
+    using System.Web.Mvc;
+
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// GET: /Home/Index/{id}
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult Login()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ WinShooter-Web2/Controllers/HomeController.cs
@@ -0,0 +1,216 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HomeController.cs" company="Copyright ©2013 John Allberg & Jonas Fredriksson">
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU General Public License
+//   as published by the Free Software Foundation; either version 2
+//   of the License, or (at your option) any later version.
+//   
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General Public License for more details.
+//   
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the Free Software
+//   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// </copyright>
+// <summary>
+//   The home controller.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WinShooter.Controllers
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// The home controller.
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// GET: /Home/Index/{id}
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult Index(string id)
+        {
+            if (id == null)
+            {
+                return this.View();
+            }
+
+            // TODO Get competition from database
+            // TODO Check user rights on database
+            this.ViewBag.CompetitionName = "Höstsaluten";
+            this.ViewBag.CompetitionGuid = id;
+
+            return this.View();
+        }
+
+        /// <summary>
+        /// The competition info.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult Competition(string id)
+        {
+            if (id == null)
+            {
+                return this.RedirectToActionPermanent("Index");
+            }
+
+            // TODO Get competition from database
+            // TODO Check user rights on database
+            this.ViewBag.CompetitionName = "Höstsaluten";
+            this.ViewBag.CompetitionGuid = id;
+            return this.View();
+        }
+
+        /// <summary>
+        /// The clubs.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult Clubs(string id)
+        {
+            if (id == null)
+            {
+                return this.RedirectToActionPermanent("Index");
+            }
+
+            // TODO Get competition from database
+            // TODO Check user rights on database
+            this.ViewBag.CompetitionName = "Höstsaluten";
+            this.ViewBag.CompetitionGuid = id;
+            return this.View();
+        }
+
+        /// <summary>
+        /// The weapons.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult Weapons(string id)
+        {
+            if (id == null)
+            {
+                return this.RedirectToActionPermanent("Index");
+            }
+
+            // TODO Get competition from database
+            // TODO Check user rights on database
+            this.ViewBag.CompetitionName = "Höstsaluten";
+            this.ViewBag.CompetitionGuid = id;
+            return this.View();
+        }
+
+        /// <summary>
+        /// The stations.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult Stations(string id)
+        {
+            if (id == null)
+            {
+                return this.RedirectToActionPermanent("Index");
+            }
+
+            // TODO Get competition from database
+            // TODO Check user rights on database
+            this.ViewBag.CompetitionName = "Höstsaluten";
+            this.ViewBag.CompetitionGuid = id;
+            return this.View();
+        }
+
+        /// <summary>
+        /// The competitors.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult Competitors(string id)
+        {
+            if (id == null)
+            {
+                return this.RedirectToActionPermanent("Index");
+            }
+
+            // TODO Get competition from database
+            // TODO Check user rights on database
+            this.ViewBag.CompetitionName = "Höstsaluten";
+            this.ViewBag.CompetitionGuid = id;
+            return this.View();
+        }
+
+        /// <summary>
+        /// The enter results.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult EnterResults(string id)
+        {
+            if (id == null)
+            {
+                return this.RedirectToActionPermanent("Index");
+            }
+
+            // TODO Get competition from database
+            // TODO Check user rights on database
+            this.ViewBag.CompetitionName = "Höstsaluten";
+            this.ViewBag.CompetitionGuid = id;
+            return this.View();
+        }
+
+        /// <summary>
+        /// The results.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult Results(string id)
+        {
+            if (id == null)
+            {
+                return this.RedirectToActionPermanent("Index");
+            }
+
+            // TODO Get competition from database
+            // TODO Check user rights on database
+            this.ViewBag.CompetitionName = "Höstsaluten";
+            this.ViewBag.CompetitionGuid = id;
+            return this.View();
+        }
+    }
+}
