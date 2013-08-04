8550927ff3e98eed229f0379db16b24b3ad1214a
John Allberg <john_allberg@hotmail.com>
2013-08-04 20:36:04 +0200

    Small updates.

--- WinShooter-Web.Api/Api/Rights.cs
+++ WinShooter-Web.Api/Api/Rights.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WinShooter.Api.Api
+﻿namespace WinShooter.Api.Api
 {
-    class Rights
+    using ServiceStack.ServiceHost;
+
+    [Route("/rights")]
+    public class Rights
     {
     }
 }
