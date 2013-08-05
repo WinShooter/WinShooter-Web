f5a030c9d075ba46d887372dccda48f3b8afa0be
John Allberg <john_allberg@hotmail.com>
2013-08-05 07:48:26 +0200

    Started implementing the role/right tables in NHibernate.

---
+++ WinShooter-Web.Database/Role.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Role.cs" company="Copyright ©2013 John Allberg & Jonas Fredriksson">
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU General Public License
+//   as published by the Free Software Foundation; either version 2
+//   of the License, or (at your option) any later version.
+//   
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General Public License for more details.
+//   
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the Free Software
+//   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// </copyright>
+// <summary>
+//   The representation of the database role.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WinShooter.Database
+{
+    /// <summary>
+    /// The representation of the database role.
+    /// </summary>
+    public class Role
+    {
+        public virtual string Id { get; set; }
+        public virtual string RoleName { get; set; }
+    }
+}
