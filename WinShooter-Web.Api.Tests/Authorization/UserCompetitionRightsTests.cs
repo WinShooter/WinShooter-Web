3a71e76ee3a2bf12b9e4c02fb7cb5bc367d7338d
John Allberg <john_allberg@hotmail.com>
2013-08-07 07:28:14 +0200

    Fixed test to pass.

--- WinShooter-Web.Api.Tests/Authorization/UserCompetitionRightsTests.cs
+++ WinShooter-Web.Api.Tests/Authorization/UserCompetitionRightsTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace WinShooter.Api.Tests.Authorization
 {
     using System;
+    using System.Collections.Generic;
 
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using WinShooter.Api.Authorization;
@@ -12,11 +13,9 @@
         [TestMethod]
         public void TestCorrectRights()
         {
-            var user = new User();
             var competitionId = Guid.Parse("731bc7fd-1ab6-49ae-8056-92b507eef5e9");
 
-            var rights = new UserCompetitionRights(competitionId, user);
-            rights.Permissions.Add("AddCompetition");
+            var rights = new UserCompetitionRights(competitionId, new List<string>() { "AddCompetition" });
             Assert.IsTrue(rights.HasPermission("AddCompetition"));
             Assert.IsFalse(rights.HasPermission("DestroyEverything"));
         }
