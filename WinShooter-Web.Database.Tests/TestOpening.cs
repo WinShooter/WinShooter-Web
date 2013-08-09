56e572eb7d8ca0161a7082e75c90a6a56703467c
John Allberg <john_allberg@hotmail.com>
2013-08-10 00:18:25 +0200

    Added test project for WinShooter-Web.Database.

---
+++ WinShooter-Web.Database.Tests/TestOpening.cs
@@ -0,0 +1,35 @@
+﻿namespace WinShooter.Database.Tests
+{
+    using System.Configuration;
+    using System.Linq;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using NHibernate.Linq;
+
+    using WinShooter_Web.DatabaseMigrations;
+
+    [TestClass]
+    public class TestOpening
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            var sqlDatabaseMigrator = new SqlDatabaseMigrator();
+            sqlDatabaseMigrator.MigrateToLatest(ConfigurationManager.ConnectionStrings["WinShooterConnection"].ConnectionString);
+        }
+
+        [TestMethod]
+        public void SmallOpeningTest()
+        {
+            using (var dbsession = NHibernateHelper.OpenSession())
+            {
+                var userLoginInfo = (from info in dbsession.Query<UserLoginInfo>()
+                                     where
+                                         info.IdentityProvider == "google" && info.IdentityProviderId == "something"
+                                     select info).SingleOrDefault();
+                Assert.IsNull(userLoginInfo);
+            }
+        }
+    }
+}
