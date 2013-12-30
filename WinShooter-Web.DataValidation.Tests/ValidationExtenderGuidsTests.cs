681e7f07dfe6031b9c477afab89fc4bb9f87b5bb
John Allberg <john_allberg@hotmail.com>
2013-12-30 07:09:47 +0100

    Added unit tests for Guid validator.

---
+++ WinShooter-Web.DataValidation.Tests/ValidationExtenderGuidsTests.cs
@@ -0,0 +1,33 @@
+﻿namespace WinShooter_Web.DataValidation.Tests
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using WinShooter.Web.DataValidation;
+
+    [TestClass]
+    public class ValidationExtenderGuidsTests
+    {
+        /// <summary>
+        /// Check that not empty validation works on <see cref="Guid"/> that is not empty.
+        /// </summary>
+        [TestMethod]
+        public void NotEmptyNotEmpty()
+        {
+            var guid = Guid.NewGuid();
+            guid.Require("Test").NotEmpty();
+        }
+
+        /// <summary>
+        /// Check that not empty validation works on <see cref="Guid"/> that is empty.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NotEmptyEmpty()
+        {
+            var guid = Guid.Empty;
+            guid.Require("Test").NotEmpty();
+        }
+    }
+}
