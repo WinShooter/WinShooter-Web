3eea1a250e61445aebeb8d4948f06de1b4f3ba6d
John Allberg <john_allberg@hotmail.com>
2013-12-30 07:15:45 +0100

    Added unit tests for the string validator.

---
+++ WinShooter-Web.DataValidation.Tests/ValidationExtenderStringsTests.cs
@@ -0,0 +1,54 @@
+﻿namespace WinShooter_Web.DataValidation.Tests
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using WinShooter.Web.DataValidation;
+
+    [TestClass]
+    public class ValidationExtenderStringsTests
+    {
+        /// <summary>
+        /// Test the shorter than requirements positive.
+        /// </summary>
+        [TestMethod]
+        public void ShorterThanShorterThan()
+        {
+            var str = new String('a', 100);
+            str.Require("str").ShorterThan(101);
+        }
+
+        /// <summary>
+        /// Test the shorter than requirement negative.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShorterThanLongerThan()
+        {
+            var str = new String('a', 100);
+            str.Require("str").ShorterThan(99);
+        }
+
+        /// <summary>
+        /// Test the longer than requirements negative.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LongerThanShorterThan()
+        {
+            var str = new String('a', 100);
+            str.Require("str").LongerThan(101);
+        }
+
+        /// <summary>
+        /// Test the longer than requirement postitive.
+        /// </summary>
+        [TestMethod]
+        public void LongerThanLongerThan()
+        {
+            var str = new String('a', 100);
+            str.Require("str").LongerThan(99);
+        }
+    }
+}
