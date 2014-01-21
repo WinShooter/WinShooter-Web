359662da12d70f2951e16e766d9235da3b58cea0
John Allberg <john_allberg@hotmail.com>
2014-01-21 07:29:01 +0100

    Revert "Split the large SQL to smaller bits."
    This reverts commit 022e7f3148b1c00107bb36786a3e29dac6c5177c.

--- WinShooter-Web.DatabaseMigrations/Migrations/Iteration1/Migration116AddRoleWeaponManagerRights.sql
+++
@@ -1,7 +0,0 @@
-﻿/* Weapon Manager */
-insert into Roles (Id, RoleName) values ('DFF79A10-37AA-4F6C-934A-4EF927556148', 'SystemWeaponManager')
-
-insert into RoleRightsInfo (RoleId, RightId) values ((select Id from Roles where RoleName='SystemWeaponManager'), (select Id from Rights where Name='CreateWeapon'))
-insert into RoleRightsInfo (RoleId, RightId) values ((select Id from Roles where RoleName='SystemWeaponManager'), (select Id from Rights where Name='ReadWeapon'))
-insert into RoleRightsInfo (RoleId, RightId) values ((select Id from Roles where RoleName='SystemWeaponManager'), (select Id from Rights where Name='UpdateWeapon'))
-insert into RoleRightsInfo (RoleId, RightId) values ((select Id from Roles where RoleName='SystemWeaponManager'), (select Id from Rights where Name='DeleteWeapon'))
