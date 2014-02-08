8203f5c3c921363fda44783f2bf02754b8b943eb
John Allberg <john_allberg@hotmail.com>
2014-02-08 14:10:18 +0100

    Added test place holders for jasmine tests.

---
+++ WinShooter-Web.Tests/Scripts/App/AngularModulesTests.js
@@ -0,0 +1,8 @@
+﻿///<reference path="~/Scripts/jasmine.js"/>
+///<reference path="~/../WinShooter-Web/Scripts/App/AngularModules.js"/>
+
+describe("AngularModules", function () {
+    it("This is a test place holder", function () {
+        expect(true).toBe(false);
+    });
+});
---
+++ WinShooter-Web.Tests/Scripts/App/cookieScriptTests.js
@@ -0,0 +1,8 @@
+﻿///<reference path="~/Scripts/jasmine.js"/>
+///<reference path="~/../WinShooter-Web/Scripts/App/cookieScript.js"/>
+
+describe("cookieScript", function () {
+    it("This is a test place holder", function () {
+        expect(true).toBe(false);
+    });
+});
