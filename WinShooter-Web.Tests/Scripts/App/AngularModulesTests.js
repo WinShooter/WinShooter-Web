ab55f12f525eb98d41db61a49dd4c8b347a14d41
John Allberg <john_allberg@hotmail.com>
2014-02-09 06:28:12 +0100

    First working test with CurrentUserController.

--- WinShooter-Web.Tests/Scripts/App/AngularModulesTests.js
+++ WinShooter-Web.Tests/Scripts/App/AngularModulesTests.js
@@ -1,40 +1,47 @@
 ﻿///<reference path="~/Scripts/jasmine.js"/>
 ///<reference path="~/../WinShooter-Web/Scripts/Angular.js"/>
-///<reference path="~/../WinShooter-Web/Scripts/angular-route.js" />
+///<reference path="~/../WinShooter-Web/Scripts/angular-resource.js" />
+///<reference path="~/../WinShooter-Web/Scripts/angular-sanitize.js"/>
 ///<reference path="~/../WinShooter-Web/Scripts/angular-mocks.js" />
 ///<reference path="~/../WinShooter-Web/Scripts/App/common.js"/>
 ///<reference path="~/../WinShooter-Web/Scripts/App/AngularModules.js"/>
 
 'use strict';
 
-angular.module('winshooter', [])
-    .value('mode', 'app')
-    .value('version', 'v1.0.1');
-
 describe("CurrentUserController", function () {
-    debugger;
+    // For now, disable resharper callback
     //jasmine.getEnv().currentRunner_.finishCallback = function () { };
-    var scope;//we'll use this scope in our tests
- 
+
+    //we'll use this scope in our tests
+    var scope;
+
     //mock Application to allow us to inject our own dependencies
     beforeEach(angular.mock.module('winshooter'));
 
-    beforeEach(angular.module('ngRoute'));
-
-    //mock the controller for the same reason and include $rootScope and $controller
-    beforeEach(angular.mock.inject(function ($rootScope, $controller, _$httpBackend_) {
-        //create an empty scope
-        scope = $rootScope.$new();
-
-        $httpBackend = _$httpBackend_;
-        $httpBackend.when('GET', 'Users/users.json').respond([{ id: 1, name: 'Bob' }, { id: 2, name: 'Jane' }]);
-
-        //declare the controller and inject our empty scope
-        $controller('CurrentUserController', { $scope: scope });
-    }));
-
     // tests start here
-    it("This is a test place holder", function () {
-        expect(true).toBe(false);
+    it('Check that the user isnt logged in', function () {
+        angular.mock.inject(function ($rootScope, $controller, $location, $modal) {
+            //create an empty scope
+            scope = $rootScope.$new();
+
+            debugger;
+
+            var currentUserFactory = jasmine.createSpyObj('currentUserFactoryStub', ['query', 'get']);
+            currentUserFactory.query.andCallFake(function() {
+                return { IsLoggedIn: false };
+            });
+
+            //declare the controller and inject our empty scope
+            var myController = $controller('CurrentUserController', {
+                $rootScope: scope,
+                $scope: scope,
+                $location: $location,
+                $modal: $modal,
+                currentUserFactory: currentUserFactory
+            });
+            expect(myController).toBeDefined();
+
+            expect(scope.currentUser.IsLoggedIn).toBe(false);
+        });
     });
 });
--- WinShooter-Web.Tests/Scripts/Tmp/home.js
+++
@@ -1,7 +0,0 @@
-﻿'use strict';
-
-var app = angular.module('Application', ['ngResource']);
-
-app.controller('MainCtrl', function ($scope) {
-    $scope.text = 'Hello World!';
-});
