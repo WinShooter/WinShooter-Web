269d746569378030fee02ed0e3c4591fbd6e33d8
John Allberg <john_allberg@hotmail.com>
2014-02-09 05:13:45 +0100

    Added a small test which actually works. :-)

---
+++ WinShooter-Web.Tests/Scripts/Tmp/home.js
@@ -0,0 +1,7 @@
+﻿'use strict';
+
+var app = angular.module('Application', ['ngResource']);
+
+app.controller('MainCtrl', function ($scope) {
+    $scope.text = 'Hello World!';
+});
---
+++ WinShooter-Web.Tests/Scripts/Tmp/home.tests.js
@@ -0,0 +1,27 @@
+﻿///<reference path="~/Scripts/jasmine.js"/>
+///<reference path="~/../WinShooter-Web/Scripts/Angular.js"/>
+///<reference path="~/../WinShooter-Web/Scripts/angular-resource.js" />
+///<reference path="~/../WinShooter-Web/Scripts/angular-mocks.js" />
+///<reference path="~/Scripts/Tmp/home.js"/>
+'use strict';
+
+describe('A dummy controller tests', function () {
+    var scope = { text: "oh" };//we'll use this scope in our tests
+
+    //mock Application to allow us to inject our own dependencies
+    beforeEach(angular.mock.module('Application'));
+
+    //mock the controller for the same reason and include $rootScope and $controller
+    beforeEach(angular.mock.inject(function ($rootScope, $controller) {
+        //create an empty scope
+        scope = $rootScope.$new();
+
+        //declare the controller and inject our empty scope
+        $controller('MainCtrl', { $scope: scope });
+    }));
+
+    // tests start here
+    it('should have variable text = "Hello World!"', function () {
+        expect(scope.text).toBe('Hello World!');
+    });
+});
