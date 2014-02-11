cfe47e1c091b4e23b31d21a83a2a7500340dd1ed
John Allberg <john_allberg@hotmail.com>
2014-02-11 20:27:43 +0100

    Resharping.

--- WinShooter-Web.Api.Tests/Api/Competitions/CompetitionRequestTests.cs
+++ WinShooter-Web.Api.Tests/Api/Competitions/CompetitionRequestTests.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="CompetitionResponseTests.cs" company="Copyright ©2014 John Allberg & Jonas Fredriksson">
+// <copyright file="CompetitionRequestTests.cs" company="Copyright ©2014 John Allberg & Jonas Fredriksson">
 //   This program is free software; you can redistribute it and/or
 //   modify it under the terms of the GNU General Public License
 //   as published by the Free Software Foundation; either version 2
