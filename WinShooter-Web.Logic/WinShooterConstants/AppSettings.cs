de6562a1b5f8d7f071685e1bbefefd7cf0ebf5e2
smuda <john@ayoy.se>
2014-12-10 05:41:24 +0100

    Enabled running stylecop on debug build.

---
+++ WinShooter-Web.Logic/WinShooterConstants/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace WinShooter.Logic.WinShooterConstants
+{
+    /// <summary>
+    /// A number of static strings to use for keys in app settings.
+    /// </summary>
+    public static class AppSettings
+    {
+        /// <summary>
+        /// The app settings key for Google Client ID.
+        /// </summary>
+        public static string GoogleOauthClientId = "GoogleOauthClientId";
+
+        /// <summary>
+        /// The app settings key for Google Client ID.
+        /// </summary>
+        public static string GoogleOauthSecret = "GoogleOauthSecret";
+    }
+}
